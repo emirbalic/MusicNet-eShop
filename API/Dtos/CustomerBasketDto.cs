efa2cfc5ac4e7a057d2268ccb0e3d46a4944945d
Emir Balic <emirbalic@gmail.com>
2020-08-21 10:42:06 +0200

    Stripe

--- API/Dtos/CustomerBasketDto.cs
+++ API/Dtos/CustomerBasketDto.cs
@@ -9,6 +9,10 @@ namespace API.Dtos
         public string Id { get; set; }
         // [Required]
         public List<BasketItemDto> Items { get; set; }
-        
+        public int? DeliveryMethodId { get; set; }
+        public string ClientSecret { get; set; }
+        public string PaymentIntentId { get; set; }
+        public decimal ShippingPrice { get; set; }
+
     }
 }
